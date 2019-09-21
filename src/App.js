e944293a0a22997652091cd164fff1ad6fa869dd
Touseef Ghazanfar <touseef.ghazanfar@gmail.com>
2019-09-21 23:17:40 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./component/create-todo-component";
+import EditTodo from "./component/edit-todo-component";
+import TodosList from "./component/todos-list-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-ligh bg-light">
+          <a className="navbar-brand" href="https://google.com" target="_blank" rel="noopener noreferrer">
+            <img src={logo} width="30" height="30" alt="codingthesmartway.com" />
+          </a>
+          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create Todo</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </div>
+    </Router>
   );
 }
 
